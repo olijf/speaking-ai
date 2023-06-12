8526928acfdcbafe35461f797eb46385ec71d30c
Olaf van der Kruk <olafvdkruk@gmail.com>
2023-06-12 13:13:54 +0200

    Fixed visulisation of the conversation.

--- Visualisation.py
+++ Visualisation.py
@@ -11,5 +11,9 @@ with open("conversation.json", "r") as file:
 rogue_ai = RogueAIStateMachine(conversation_script)
 
 # Create a GraphMachine instance and generate the graph
-graph = GraphMachine(rogue_ai)
+graph = GraphMachine(model=rogue_ai, states=list(rogue_ai.states.keys()), initial='intro', auto_transitions=True, title="Rogue AI State Machine", show_conditions=True)
+for state, state_data in rogue_ai.states.items():
+    for option in state_data["options"]:
+        graph.add_transition(trigger="answer", source=state, dest=option["transition"], label=option["text"])
+
 graph.get_graph().draw("conversation_graph.png", prog="dot", format="png")
